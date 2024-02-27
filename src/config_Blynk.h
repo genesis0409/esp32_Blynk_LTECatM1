919375364c154a99253dd8d087ed3160f13f4984
Daon_Sori <smcdidcksthf@naver.com>
2024-02-27 16:23:57 +0900

    gitignore: config_Blynk.h

--- src/config_Blynk.h
+++
@@ -1,5 +0,0 @@
-#define BLYNK_TEMPLATE_ID "TMPL6MS6btZfW"
-#define BLYNK_TEMPLATE_NAME "Temp SoilM EC"
-#define BLYNK_AUTH_TOKEN "f5XIvNr_EdZSWJidGU7163s3LOgIwvnz"
-// #define WIFI_SSID "dinfo"
-// #define WIFI_PASS "daon7521"
